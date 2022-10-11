18fc27b590ac2c7f041bff7fe31a6fca667cf041
Salma <ssalah@dev-10.com>
2022-10-11 10:34:26 -0500

    security

--- resume-builder-server/src/main/java/learn/resume/builder/data/AppUserRepo.java
+++ resume-builder-server/src/main/java/learn/resume/builder/data/AppUserRepo.java
@@ -1,9 +1,14 @@
 package learn.resume.builder.data;
 
+import learn.resume.builder.domain.Result;
 import learn.resume.builder.models.AppUser;
 
 public class AppUserRepo {
     public AppUser findByUsername(String username) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Result<AppUser> create(AppUser appUser) {
         throw new UnsupportedOperationException();
     }
 }
