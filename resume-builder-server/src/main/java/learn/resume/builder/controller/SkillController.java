bacb4fc9f6d8b492669b184edd4f18f55a0d7081
Salma <ssalah@dev-10.com>
2022-10-12 10:00:22 -0500

    fixes

--- resume-builder-server/src/main/java/learn/resume/builder/controller/SkillController.java
+++ resume-builder-server/src/main/java/learn/resume/builder/controller/SkillController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000"})
-@RequestMapping("/view_resume")
+@RequestMapping("/api/skill")
 public class SkillController {
 
     private final SkillService service;
