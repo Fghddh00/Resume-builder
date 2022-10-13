7356e86e06a03e263be384c8e47c5edc9697e10c
unknown <jasonminoh@gmail.com>
2022-10-13 09:37:47 -0500

    updtaed resume table

--- resume-builder-server/src/main/java/learn/resume/builder/data/mapper/ResumeMapper.java
+++ resume-builder-server/src/main/java/learn/resume/builder/data/mapper/ResumeMapper.java
@@ -11,16 +11,10 @@ import java.sql.SQLException;
 public class ResumeMapper implements RowMapper<Resume> {
     @Override
     public Resume mapRow(ResultSet rs, int rowNum) throws SQLException {
+       
         Resume resume = new Resume();
         resume.setResumeId(rs.getInt("resume_id"));
         resume.setTemplateId(rs.getInt("template_id"));
-
-
-       AppUserMapper appUserMapper = new AppUserMapper();
-
-        AppUserInfoMapper appUserInfoMapper = new AppUserInfoMapper();
-        resume.setUserInfo(appUserInfoMapper.mapRow(rs, rowNum));
-
 
         return resume;
     }
