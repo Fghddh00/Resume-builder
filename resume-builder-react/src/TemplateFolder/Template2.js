97eecfa3615c2dc2fb7d859331d521a5a2ad6c9f
avis ebron <avisebron@gmail.com>
2022-10-18 16:41:47 -0400

    download complete

--- resume-builder-react/src/TemplateFolder/Template2.js
+++ resume-builder-react/src/TemplateFolder/Template2.js
@@ -1,6 +1,7 @@
 
 import { Cell, Grid, GridContainer } from "react-foundation";
 import Education from "../Education/Education";
+import Skill from "../Skill/Skill";
 import WorkHistory from "../WorkHistory/WorkHistory";
 import "./template2.css";
 
@@ -16,48 +17,53 @@ function Template2({educations,skills, workHistories, userInfo }){
     return(
         <GridContainer className="template2">
         <Grid >
-            <Cell className="nameHeader">
-            <h1 >{userInfo.firstName} {userInfo.lastName}</h1>
-            <a href={`mailto:${userInfo.email}`}> {userInfo.email} </a>
+            <div className="page">
+            <Cell>
+            <h1 className="nameHeader2">{userInfo.firstName} {userInfo.lastName}</h1>
+            <ul id="userInformation">{userInfo.email}</ul>
+            <ul id="userInformationMargin">{userInfo.address}</ul>
+            <ul id="userInformationMargin">{userInfo.phoneNumber}</ul>
             </Cell>
-        </Grid>
-        <Grid>
-            <h3>Education</h3>
-            <Cell className="education2">
-            <ul>
-                {educations.map(s=>
-                    <li>
-                    <Education
-                    schoolName={s.schoolName}
-                    educationLevel={s.educationLevel}
-                    /></li>)}
+            <Cell>
+            <h5>Education</h5>
+            
+            <ul  >{educations.map(e =>
+                       <li className="resumeInfo"><Education 
+                       schoolName={e.schoolName}
+                       educationLevel={e.educationLevel}
+                       /></li>
+            )}
+           </ul>
+           
+           </Cell>
+            <hr></hr>
+            <Cell>
+            <h5 >Work History</h5>
+            <ul >{workHistories.map(w =>
+                <li className="resumeInfo"><WorkHistory 
+                company={w.company} 
+                jobTitle={w.jobTitle}
+                startDate={w.startDate}
+                endDate={w.endDate}
+                jobDescription={w.jobDescription}
+                /></li>
+                )}
             </ul>
             </Cell>
-            <h3>Work History</h3>
-            <Cell className="WrokHistory2">
-            <ul>
-                {workHistories.map(s=>
-                    <li>
-                    <WorkHistory
-                    company={s.company}
-                    jobTitle={s.jobTitle}
-                    startDate={s.startDate}
-                    endDate={s.endDate}
-                    jobDescription={s.jobDescription}
-                    /></li>)}
-            </ul>
+            <hr></hr>
+            <Cell>
+            <h5 className="skill2">Skills:</h5>
+            <ul>{skills.map(s =>
+                <Skill 
+                skillName={s.skillName}
+                />
+            )}</ul>
             </Cell>
-            <Cell className="WrokHistory2">
-            <ul>
-                {skills.map(s=>
-                    <text>
-                     {s.skillName}
-                     </text>  )}
-            </ul>
-            </Cell>
-        </Grid>
+             </div>
 
+             
 
+        </Grid>
         </GridContainer>
     );
 }
